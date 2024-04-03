acaaf8d080c7a348b1bf397e8df84cfc61e1a8b7
dhiraj nande <dhiraj.nande@smartgig.tech>
2024-04-03 16:28:01 +0530

    first1

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/frontend/UI/PMT-frontend/
+cd /home/ubuntu/frontend/UI/src/
 sudo docker build -t pmt_frontend1 .
 sudo docker compose up -d
--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/frontend/UI/PMT-frontend/
+cd /home/ubuntu/frontend/UI/src/
 sudo docker compose down
 sudo docker rmi pmt_frontend1
